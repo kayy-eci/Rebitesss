070d43572aa8673a0a07fc7781cf2af2b0024cc6
kayysan <jokyo931@gmail.com>
2026-08-28 10:55:49 +0700

    felentino komen

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ const TESTIMONIALS: Testimonial[] = [
       "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop",
   },
   {
-    name: "Falen Darmawan",
+    name: "Falentino",
     role: "Mahasiswa",
     quote:
       "Sebagai anak kos, ReBites cukup ngebantu sih. Bisa dapet makanan yang masih bagus dengan harga lebih murah. Biasanya sebelum beli makan saya cek sini dulu.",
